cb5c3ccaac9d64daa8ac632e1715fa41e3ac53eb
YashPorwal23 <144919496+YashPorwal23@users.noreply.github.com>
2026-02-11 22:42:53 +0530

    [3668] [Time Beats: 99.91%] [Memory Beats: 21.64%] - LeetPush

--- Restore-Finishing-Order.java
+++ Restore-Finishing-Order.java
@@ -1,12 +1,12 @@
 1class Solution {
 2    public int[] recoverOrder(int[] order, int[] friends) {
-3        HashSet<Integer> set = new HashSet<>();
-4        for(int x:friends){
-5            set.add(x);
+3        int freq[] = new int[101];
+4        for(int x:friends){ 
+5            freq[x]++;
 6        }
 7        int c=0;
 8        for(int i=0;i<order.length;i++){
-9            if(set.contains(order[i])){
+9            if(freq[order[i]]!=0){
 10                friends[c++]=order[i];
 11            }
 12        }
