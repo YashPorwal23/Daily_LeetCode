7a6e63c5f424afa64bd0365b5527de86587e643a
YashPorwal23 <144919496+YashPorwal23@users.noreply.github.com>
2026-02-13 15:00:18 +0530

    [3834] [Time Beats: 50.74%] [Memory Beats: 6.75%] - LeetPush

--- Merge-Adjacent-Equal-Elements.java
+++ Merge-Adjacent-Equal-Elements.java
@@ -3,21 +3,16 @@
 3      ArrayDeque<Long> st = new ArrayDeque<>();
 4      for(int i=0;i<nums.length;i++){
 5        long sum = nums[i];
-6        if(st.isEmpty()||sum!=st.peek()){
-7            st.push(sum);
+6        while(!st.isEmpty()&&sum==st.peek()){
+7            sum = sum+st.pop();
 8        }
-9        else{
-10        while(!st.isEmpty()&&sum==st.peek()){
-11            sum = sum+st.pop();
-12        }
-13        st.push(sum);
+9        st.push(sum);
+10      }
+11      ArrayList<Long> al = new ArrayList<>();
+12      while(!st.isEmpty()){
+13        al.add(st.pop());
 14      }
-15      }
-16      ArrayList<Long> al = new ArrayList<>();
-17      while(!st.isEmpty()){
-18        al.add(st.pop());
-19      }
-20      Collections.reverse(al);
-21      return al; 
-22    }
-23}
+15      Collections.reverse(al);
+16      return al; 
+17    }
+18}
