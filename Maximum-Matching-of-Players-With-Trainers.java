529207c98f3cc01143d8a16944fbea2e1fc69521
YashPorwal23 <144919496+YashPorwal23@users.noreply.github.com>
2026-02-18 11:30:50 +0530

    [2410] [Time Beats: 81.87%] [Memory Beats: 98.00%] - LeetPush

---
+++ Maximum-Matching-of-Players-With-Trainers.java
@@ -0,0 +1,20 @@
+1class Solution {
+2    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+3        Arrays.sort(players);
+4        Arrays.sort(trainers);
+5        int st = 0;
+6        int end = 0;
+7        int ans = 0;
+8        while(st<players.length && end<trainers.length){
+9            if(players[st]<=trainers[end]){
+10                ans++;
+11                st++;
+12                end++;
+13            }
+14            else if(players[st]>trainers[end]){
+15                end++;
+16            }
+17        }
+18        return ans;
+19    }
+20}
