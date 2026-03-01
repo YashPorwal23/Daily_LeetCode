a61d2ab9678394fb28dbe7eec930afe19c6a268e
YashPorwal23 <144919496+YashPorwal23@users.noreply.github.com>
2026-03-01 16:48:17 +0530

    [1689] [Time Beats: 60.00%] [Memory Beats: 86.42%] - LeetPush

---
+++ Partitioning-Into-Minimum-Number-Of-Deci-Binary-Numbers.java
@@ -0,0 +1,9 @@
+1class Solution {
+2    public int minPartitions(String n) {
+3        int ans = 0;
+4        for(int i=0;i<n.length();i++){
+5            ans=Math.max(ans,n.charAt(i)-'0');
+6        }
+7        return ans;
+8    }
+9}
