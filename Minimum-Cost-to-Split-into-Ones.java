54d1e0036437c01c18206760382ef7257d242c4a
YashPorwal23 <144919496+YashPorwal23@users.noreply.github.com>
2026-03-05 11:57:33 +0530

    [3857] [Time Beats: 100.00%] [Memory Beats: 18.93%] - LeetPush

--- Minimum-Cost-to-Split-into-Ones.java
+++ Minimum-Cost-to-Split-into-Ones.java
@@ -1,10 +1,5 @@
 1class Solution {
 2    public int minCost(int n) {
-3       int c = 0;
-4       while(n>1){
-5        n=1*n-1;
-6        c+=n; 
-7       } 
-8       return c;
-9    }
-10}
+3      return (n*(n-1))/2;
+4    }
+5}
