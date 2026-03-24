71e4cd701fe844f33a68bdf3cdb3e734840d0257
YashPorwal23 <144919496+YashPorwal23@users.noreply.github.com>
2026-03-24 21:57:17 +0530

    [521] [Time Beats: 100.00%] [Memory Beats: 31.20%] - LeetPush

---
+++ Longest-Uncommon-Subsequence-I.java
@@ -0,0 +1,8 @@
+1class Solution {
+2    public int findLUSlength(String a, String b) {
+3        if(a.equals(b)){
+4            return -1;
+5        }
+6        return Math.max(a.length(),b.length());
+7    }
+8}
