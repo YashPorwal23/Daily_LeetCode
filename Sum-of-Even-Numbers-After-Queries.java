0f4d53478f408cbf6b6fa774c996d4a45afc0bf7
YashPorwal23 <144919496+YashPorwal23@users.noreply.github.com>
2026-03-31 22:21:41 +0530

    [985] [Time Beats: 74.43%] [Memory Beats: 41.48%] - LeetPush

--- Sum-of-Even-Numbers-After-Queries.java
+++ Sum-of-Even-Numbers-After-Queries.java
@@ -1,19 +1,30 @@
 1class Solution {
 2    public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
 3        int ans[] = new int[queries.length];
-4        int idx = 0;
-5        for(int i=0;i<queries.length;i++){
-6            int id = queries[i][1];
-7            int val = queries[i][0];
-8            int sum = 0;
-9            nums[id]+=val;
-10            for(int j=0;j<nums.length;j++){
-11                if(nums[j]%2==0){
-12                    sum+=nums[j];
-13                }
-14            }
-15            ans[idx++] = sum;
-16        }
-17        return ans;
-18    }
-19}
+4        int sum = 0;
+5        for(int i=0;i<nums.length;i++){
+6            if(nums[i]%2==0){
+7                sum+=nums[i];
+8            }
+9        }
+10        for(int i=0;i<queries.length;i++){
+11            int val = queries[i][0];
+12            int idx = queries[i][1];
+13            if((nums[idx]+val)%2==0){
+14                if(nums[idx]%2==0){
+15                    sum-=nums[idx];
+16                }
+17                nums[idx]+=val;
+18                sum+=nums[idx];
+19            }
+20            else{
+21                if(nums[idx]%2==0){
+22                    sum-=nums[idx];
+23                }
+24                nums[idx]+=val;
+25            }
+26            ans[i]=sum;
+27        }
+28        return ans;
+29    }
+30}
